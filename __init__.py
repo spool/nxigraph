a9031c940fcae8fd8e4e086f7ada01dd4a97f5ae
Griffith Rees <griff.rees@gmail.com>
2009-04-22 16:53:09 +0100

    Added doctest to insure nx changes propagate to igraph changes.

--- __init__.py
+++ __init__.py
@@ -1,24 +1,46 @@
 """
 Provides a toolkit for interoperability between igraph and networkx
 """
-
 import networkx
 import igraph
+
+def nx2igraph(nxgraph):
+    """
+    Converts a networkx graph to an igraph graph.
+    """
+    d = {}
+    adj = nxgraph.adj
+    for i, k in enumerate(adj.keys()):
+        d[k] = i
+    l = [(d[t[0]], d[t[1]]) for t in nxgraph.edges()]
+    return(igraph.Graph(l))
 
 class SGraph(networkx.Graph):
     """
     Extends the networkx Graph class with an igraph property.
 
-    g = SGraph()
-    g.igraph.average_path_length()
+    >>> g = SGraph()
+    >>> edgelist = [('blue', 'purple'),
+    ...                 ('blue', 'green'),
+    ...                 ('blue', 'red'),
+    ...                 ('yellow', 'green'),
+    ...                 ('green', 'orange')]
+    >>> g.add_edges_from(edgelist)
+    >>> networkx.diameter(g)
+    3
+    >>> g.igraph.diameter()
+    3
+    >>> g.add_edge('orange', 'mauve')
+    >>> networkx.diameter(g)
+    4
+    >>> g.igraph.diameter()
+    4
     """
-
     @property
     def igraph(self):
-        d = {}
-        adj = self.adj
-        for i, k in enumerate(adj.keys()):
-            d[k] = i
-        l = [(d[t[0]], d[t[1]]) for t in self.edges()]
-        g = igraph.Graph(l)
-        return g
+        return nx2igraph(self)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+
